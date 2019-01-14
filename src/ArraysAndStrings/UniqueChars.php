e385f46cc47eba20eb1571bb8cdc043cbd524ace
Hovsep <avakyan.hovsep@yandex.ru>
2019-01-14 18:31:44 +0300

    Minor

--- src/ArraysAndStrings/UniqueChars.php
+++ src/ArraysAndStrings/UniqueChars.php
@@ -3,9 +3,10 @@
 namespace ArraysAndStrings;
 
 /**
+ * Question 1.1
+ *
  * Implement an algorithm to determine if a string has all unique characters.
  * What if you can not use additional data structures?
- *
  *
  * Class UniqueChars
  * @package ArraysAndStrings
