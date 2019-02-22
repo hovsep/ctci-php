65c598cbe4506281c3bf47401590a17782efb10e
Hovsep <avakyan.hovsep@yandex.ru>
2019-02-22 23:01:30 +0300

    +Question 2.3

---
+++ src/LinkedLists/DeleteMiddleNode.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hovsep
+ * Date: 22.02.19
+ * Time: 22:56
+ */
+
+namespace LinkedLists;
+
+/**
+ * Question 2.3
+ * Implement an algorithm to delete a node in the middle (i.e., any node but
+ * the first and last node, not necessarily the exact middle) of a singly linked list, given only access to
+ * that node.EXAMPLE
+ * input:the node c from the linked list a->b->c->d->e->f
+ * Result: nothing is returned, but the new linked list looks like a->b->d->e->f
+ *
+ * Class DeleteMiddleNode
+ * @package LinkedLists
+ */
+class DeleteMiddleNode
+{
+
+    public static function deleteMiddleNode(MyLinkedListNode $nodeToDelete): void
+    {
+        $current = $nodeToDelete;
+
+        while(!is_null($current->next)) {
+
+            $current->data = $current->next->data;
+
+            if (is_null($current->next->next)) {
+                $current->next = null;
+                break;
+            }
+
+            $current = $current->next;
+        }
+    }
+}
---
+++ tests/LinkedLists/DeleteMiddleNodeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . '/../../src/LinkedLists/MyLinkedListNode.php';
+require_once __DIR__ . '/../../src/LinkedLists/DeleteMiddleNode.php';
+
+
+use LinkedLists\DeleteMiddleNode;
+
+class ReturnKthFromLastTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testWithMiddle()
+    {
+        $n3 = new \LinkedLists\MyLinkedListNode(3, null);
+        $n2 = new \LinkedLists\MyLinkedListNode(2, $n3);
+        $n1 = new \LinkedLists\MyLinkedListNode(1, $n2);
+
+        $m2 = new \LinkedLists\MyLinkedListNode(3, null);
+        $m1 = new \LinkedLists\MyLinkedListNode(1, $m2);
+
+        DeleteMiddleNode::deleteMiddleNode($n2);
+
+        $this->assertEquals((string) $m1, (string) $n1);
+    }
+
+
+}
